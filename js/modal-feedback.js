b46921139ec1b90ee7ea7a807455b53f0373631f
Alexey Leetovskiy <a.leetovskiy@gmail.com>
2020-07-30 00:40:12 +0500

    Добавлены анимации для модального окна

--- js/modal-feedback.js
+++ js/modal-feedback.js
@@ -1,6 +1,8 @@
 let modalFeedback = document.querySelector('.modal-feedback')
 let openButton = document.querySelector('.write-us-btn')
 let closeButton = modalFeedback.querySelector('.modal-close')
+let submitButton = modalFeedback.querySelector('.button[type="submit"]')
+let requiredItems = Array.prototype.slice.call(modalFeedback.querySelectorAll('*[required]'), 0);
 
 
 /* Появление и закрытие окна по кнопкам */
@@ -9,13 +11,26 @@ openButton.addEventListener('click', function() {
 })
 
 closeButton.addEventListener('click', function() {
-  modalFeedback.classList.remove('modal-show')
+
+  modalFeedback.classList.remove('modal-shake', 'modal-show')
 })
 
 
 /* Закрытие окна на ESC */
 window.addEventListener('keydown', function( evt ) {
   if (evt.keyCode == 27 && modalFeedback.classList.contains('modal-show')) {
-    modalFeedback.classList.remove('modal-show')
+    evt.preventDefault()
+
+    modalFeedback.classList.remove('modal-shake', 'modal-show')
   }
+})
+
+submitButton.addEventListener('click', function( evt) {
+  requiredItems.forEach(function(entry) {
+    if (!entry.value){
+      modalFeedback.classList.remove('modal-shake')
+      modalFeedback.offsetWidth = modalFeedback.offsetWidth
+      modalFeedback.classList.add('modal-shake')
+    }
+  });
 })
